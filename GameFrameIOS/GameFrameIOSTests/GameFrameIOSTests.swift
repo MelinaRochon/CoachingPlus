52b69f5c1aeabf7bffbec46f99ae4d66b9308ef6
Melina Rochon <mroch072@uottawa.ca>
2025-01-30 17:16:30 -0500

    Created the IOS app project.

---
+++ GameFrameIOS/GameFrameIOSTests/GameFrameIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  GameFrameIOSTests.swift
+//  GameFrameIOSTests
+//
+//  Created by Mélina Rochon on 2025-01-30.
+//
+
+import Testing
+@testable import GameFrameIOS
+
+struct GameFrameIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
