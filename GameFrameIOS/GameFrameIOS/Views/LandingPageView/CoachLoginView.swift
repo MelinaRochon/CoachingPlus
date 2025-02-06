2612a787beee50d5e7733f21bbdfc9ef66980bf4
Melina Rochon <mroch072@uottawa.ca>
2025-02-05 22:08:24 -0500

    Added landing page views

---
+++ GameFrameIOS/GameFrameIOS/Views/LandingPageView/CoachLoginView.swift
@@ -0,0 +1,18 @@
+//
+//  CoachLoginView.swift
+//  GameFrameIOS
+//
+//  Created by Mélina Rochon on 2025-02-05.
+//
+
+import SwiftUI
+
+struct CoachLoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoachLoginView()
+}
--- GameFrameIOS/GameFrameIOS/Views/LandingPageView/PlayerCreateAccountView.swift
+++ GameFrameIOS/GameFrameIOS/Views/LandingPageView/PlayerCreateAccountView.swift
@@ -1,19 +1,18 @@
 //
-//  CoachPlayerProfileView.swift
+//  PlayerCreateAccountView.swift
 //  GameFrameIOS
-//  Coach views the player's profile!
-//  Testing something!
+//
 //  Created by Mélina Rochon on 2025-02-05.
 //
 
 import SwiftUI
 
-struct CoachPlayerProfileView: View {
+struct PlayerCreateAccountView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    CoachPlayerProfileView()
+    PlayerCreateAccountView()
 }
