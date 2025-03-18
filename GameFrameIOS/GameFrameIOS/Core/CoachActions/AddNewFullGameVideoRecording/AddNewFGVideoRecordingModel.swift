e307c95a9aa91da7370b35c509e2bb131a8aabe3
Caterina Bosi <caterinarmbosi@gmail.com>
2025-03-17 20:39:22 -0400

    Added video recording model

---
+++ GameFrameIOS/GameFrameIOS/Core/CoachActions/AddNewFullGameVideoRecording/AddNewFGVideoRecordingModel.swift
@@ -0,0 +1,16 @@
+//
+//  AddNewFGVideoRecordingModel.swift
+//  GameFrameIOS
+//
+//  Created by Caterina Bosi on 2025-03-13.
+//
+
+import Foundation
+import SwiftUI
+
+@MainActor
+final class AddNewFGVideoRecordingModel: ObservableObject {
+    @Published var fullGameVideoRecording: DBFullGameVideoRecording? = nil
+    
+    @Published var 
+}
