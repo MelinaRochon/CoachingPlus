f99f76f3ab1be6823224f294edbd939b7ebefb15
Melina Rochon <mroch072@uottawa.ca>
2025-03-26 17:33:15 -0400

    Added the transcripts and key moments links on the player's side

--- GameFrameIOS/GameFrameIOS/Views/PlayerView/MyTeamPageView/PlayerSpecificTranscriptView.swift
+++ GameFrameIOS/GameFrameIOS/Views/PlayerView/MyTeamPageView/PlayerSpecificTranscriptView.swift
@@ -8,71 +8,104 @@
 import SwiftUI
 
 struct PlayerSpecificTranscriptView: View {
+    @State var gameId: String // game Id
+    @State var teamDocId: String // team document id
+    @State var recording: keyMomentTranscript?
+    @State private var isEditing: Bool = false
+    
+    @StateObject private var viewModel = TranscriptViewModel()
+    
     var body: some View {
+        NavigationView {
             ScrollView {
-                VStack {
-                    VStack (alignment: .leading) {
-                        HStack(spacing: 0) {
-                            Text("Game X VS Y").font(.title2)
-                            Spacer()
-                            
-                        }
-                        HStack {
-                            Text("Transcript #").font(.headline)
-                            Spacer()
-                        }.padding(.bottom, -2)
-                        
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Text("Team 1").font(.subheadline).foregroundStyle(.black.opacity(0.9))
-                                Text("dd/mm/yyyy hh:mm:ss").font(.subheadline).foregroundStyle(.secondary)
-                            }
-                            Spacer()
-                            // Edit Icon
-                            Button(action: {}) {
-                                Image(systemName: "pencil.and.outline")
-                                    .foregroundColor(.blue) // Adjust color
-                            }
-                            // Share Icon
-                            Button(action: {}) {
-                                Image(systemName: "square.and.arrow.up")
-                                    .foregroundColor(.blue) // Adjust color
-                            }
-                        }
-                    }.padding(.leading).padding(.trailing).padding(.top, 3)
-                    
-                    Divider().padding(.vertical, 2)
-                    
-                    // Transcription section
-                    VStack(alignment: .leading) {
-                        Text("hh:mm:ss").font(.headline).multilineTextAlignment(.leading).frame(maxWidth: .infinity, alignment: .leading).padding(.horizontal).padding(.bottom, 2)
-                        Text("\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\"").font(.caption).multilineTextAlignment(.leading).frame(maxWidth: .infinity, alignment: .leading).padding(.horizontal)
-                    }.padding(.bottom, 10).padding(.top)
-                    
-                    
-                    // Feedback for section
-                    VStack(alignment: .leading) {
-                        Text("Feedback For").font(.headline).multilineTextAlignment(.leading).frame(maxWidth: .infinity, alignment: .leading).padding(.top, 10)
-                        
-                        HStack {
-                            Image(systemName: "person.circle").resizable().frame(width: 22, height: 22).foregroundStyle(.gray).padding(.vertical, 5)
-                            Text("John Doe")
-                        }
-                    }.padding(.horizontal)
+                if let game = viewModel.game {
                     
                     VStack {
-                        Divider()
+                        VStack (alignment: .leading) {
+                            HStack(spacing: 0) {
+                                Text(game.title).font(.title2)
+                                Spacer()
+                                
+                            }
+                            if let recording = recording {
+                                HStack {
+                                    Text("Transcript #\(recording.id+1)").font(.headline)
+                                    Spacer()
+                                }.padding(.bottom, -2)
+                            }
                             
-                        // Comment section
-                        CommentSectionView()
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    if let team = viewModel.team {
+                                        Text(team.name).font(.subheadline).foregroundStyle(.black.opacity(0.9))
+                                    }
+                                    if let recording = recording {
+                                        Text(recording.frameStart.formatted(.dateTime.year().month().day().hour().minute())).font(.subheadline).foregroundStyle(.secondary)
+                                    }
+                                }
+                                Spacer()
+                            }
+                        }.padding(.leading).padding(.trailing).padding(.top, 3)
                         
-                    }.frame(maxHeight: .infinity, alignment: .bottom)
+                        Divider().padding(.vertical, 2)
+                        
+                        // Transcription section
+                        if let recording = recording {
+                            // Transcription section
+                            VStack(alignment: .leading) {
+                                if let gameStartTime = viewModel.gameStartTime {
+                                    let durationInSeconds = recording.frameStart.timeIntervalSince(gameStartTime)
+                                    Text(formatDuration(durationInSeconds)).font(.headline).multilineTextAlignment(.leading).frame(maxWidth: .infinity, alignment: .leading).padding(.horizontal).padding(.bottom, 2)
+                                    
+                                    Text("\(recording.transcript)").font(.caption).multilineTextAlignment(.leading).frame(maxWidth: .infinity, alignment: .leading).padding(.horizontal)
+                                }
+                            }.padding(.bottom, 10).padding(.top)
+                            
+                            if let feedbackFor = viewModel.feedbackFor {
+                                // Feedback for section
+                                VStack(alignment: .leading) {
+                                    Text("Feedback For").font(.headline).multilineTextAlignment(.leading).frame(maxWidth: .infinity, alignment: .leading).padding(.top, 10)
+                                    ForEach(feedbackFor, id: \.playerId) { player in
+                                        HStack {
+                                            Image(systemName: "person.circle").resizable().frame(width: 22, height: 22).foregroundStyle(.gray).padding(.vertical, 5)
+                                            Text(player.name)
+                                        }.tag(player.playerId as String)
+                                    }
+                                }.padding(.horizontal)
+                            }
+                            VStack {
+                                Divider()
+                                
+                                // Comment section
+                                CommentSectionView()
+                                
+                            }
+                            .frame(maxHeight: .infinity, alignment: .bottom)
+                        }
+                    }
+                }
+            }
+            .task {
+                do {
+                    try await viewModel.loadGameDetails(gameId: gameId, teamDocId: teamDocId)
+                    let feedbackFor = recording!.feedbackFor ?? []
+                    
+                    try await viewModel.getFeebackFor(feedbackFor: feedbackFor)
+                } catch {
+                    print("Error when fetching specific footage info: \(error)")
                 }
             }
         }
     }
-
+    
+    func formatDuration(_ duration: TimeInterval) -> String {
+        let hours = Int(duration) / 3600
+        let minutes = (Int(duration) % 3600) / 60
+        let seconds = Int(duration) % 60
+        return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+    }
+}
 
 #Preview {
-    PlayerSpecificTranscriptView()
+    PlayerSpecificTranscriptView(gameId: "", teamDocId: "", recording: nil)
 }
